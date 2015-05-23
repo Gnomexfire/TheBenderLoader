3afeb413c4d51aab747a410806cdb452b97921ec
GnomexFire <rafael_mottarp@live.com>
2015-05-23 03:06:49 -0300

    up project

---
+++ TheBenderLoader/TheBenderLoader/Class/Util.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBenderLoader.Class
+{
+    public static class Util
+    {
+        public static string GetMd5HashOfAFile(string file)
+        {
+            var md5 = new MD5CryptoServiceProvider();
+            var stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read, 8192);
+            md5.ComputeHash(stream);
+            stream.Close();
+            var hash = md5.Hash;
+            var sb = new StringBuilder();
+            foreach (var b in hash)
+            {
+                sb.Append(string.Format("{0:X2}", b));
+            }
+            return sb.ToString();
+        }
+
+        
+
+
+    }
+}
---
+++ TheBenderLoader/TheBenderLoader/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using TheBenderLoader.Class;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace TheBenderLoader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            var buff = new StringBuilder(nChars);
+            var handle = GetForegroundWindow();
+            return GetWindowText(handle, buff, nChars) > 0 ? buff.ToString() : null;
+        }
+
+        #region Helpers
+        protected virtual string ProcessExecutablePath(Process process)
+        {
+            try
+            {
+                return process.MainModule.FileName;
+            }
+            catch
+            {
+                string query = "SELECT ExecutablePath, ProcessID FROM Win32_Process";
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+                foreach (ManagementObject item in searcher.Get())
+                {
+                    object id = item["ProcessID"];
+                    object path = item["ExecutablePath"];
+
+                    if (path != null && id.ToString() == process.Id.ToString())
+                    {
+                        return path.ToString();
+                    }
+                }
+            }
+
+            return "";
+        }
+
+
+        #endregion
+        private  readonly BackgroundWorker _startCoding = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
